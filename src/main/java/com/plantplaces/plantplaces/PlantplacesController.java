4a8cd5261f9b46231de12d2beda3816e2f89406c
Administrator <Administrator@DESKTOP-099UQRG>
2020-09-15 14:58:16 -0400

    add javadoc, add / endpoint

--- src/main/java/com/plantplaces/plantplaces/PlantplacesController.java
+++ src/main/java/com/plantplaces/plantplaces/PlantplacesController.java
@@ -3,13 +3,27 @@ package com.plantplaces.plantplaces;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author Administrator
+ * handle the /start endpoint
+ * *@return
+ */
 @Controller
 public class PlantplacesController {
+	
+	
 	
 	@RequestMapping("/start")
 	public String Start(){
 		
 		return "start";
+		
+	}
+	
+	@RequestMapping("/index")
+	public String Index(){
+		
+		return "index";
 		
 	}
 
