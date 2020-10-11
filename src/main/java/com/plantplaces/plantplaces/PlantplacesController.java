88e9408a39faca853d62fe427608722cdf95c26f
Administrator <Administrator@DESKTOP-099UQRG>
2020-10-11 15:54:50 -0400

    Final chunk of code for Assignment0

--- src/main/java/com/plantplaces/plantplaces/PlantplacesController.java
+++ src/main/java/com/plantplaces/plantplaces/PlantplacesController.java
@@ -1,7 +1,17 @@
 package com.plantplaces.plantplaces;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.plantplaces.dto.SpecimenDTO;
+import com.plantplaces.service.ISpecimenService;
+import com.plantplaces.service.SpecimenServiceStub;
 
 /**
  * @author Administrator
@@ -9,12 +19,61 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * *@return
  */
 @Controller
+
 public class PlantplacesController {
 	
+	@Autowired
+	private ISpecimenService specimenServiceStub;
 	
 	
-	@RequestMapping("/start")
-	public String Start(){
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET)
+	public SpecimenDTO read(Model model){
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchbyId(43);
+		model.addAttribute("specimenDTO", specimenDTO);
+		return specimenDTO;
+		
+	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, headers= {"content-type=text/json"})
+	public String readJSON(){
+		
+		return "start";
+		
+	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET)
+	public String addSpecimen(Model model, @RequestParam(value="latitude", required=false, defaultValue="0.0") String latitude){
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchbyId(43);
+		specimenDTO.setLatitude(latitude);
+		model.addAttribute("specimenDTO", specimenDTO);
+		return "start";
+		
+	}
+	
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, params= {"loyalty=blue"})
+	public String readBlue(){
+		
+		return "start";
+		
+	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, params= {"loyalty=silver"})
+	public ModelAndView readSilver(){
+		
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchbyId(43);
+		specimenDTO.setSpecimenID(90);
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("start");
+		modelAndView.addObject("specimenDTO", specimenDTO);
+		
+		return modelAndView;
+		
+	}
+	
+	@PostMapping("/start")
+	public String create(){
 		
 		return "start";
 		
